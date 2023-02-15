0125ba72b503b87e9ec9ad0c2af748e2194f9be4
Babsrm <babsruizm@gmail.com>
2023-02-15 13:02:33 +0100

    ejercicio 3 completado

--- src/ejercicios/Ej03MostrarSucesionFibonacci.java
+++ src/ejercicios/Ej03MostrarSucesionFibonacci.java
@@ -1,10 +1,26 @@
 package ejercicios;
 
+import funciones.LibreriaFunciones;
+
 public class Ej03MostrarSucesionFibonacci {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+//		 Crear un algoritmo que muestre los primeros 10 números de la 
+//		 sucesión de Fibonacci. La sucesión comienza con los números 0 y 1 y, 
+//		 a partir de éstos, cada elemento es la suma de los dos números 
+//		 anteriores en la secuencia: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55…
+		int vectorFibo[];
+		int longitud = 10;
+		
+		vectorFibo = new int [longitud];
+		vectorFibo[0]=0;
+		vectorFibo[1]=1;
+		
+		for (int i = 2; i < longitud; i++) {
+			vectorFibo[i] = vectorFibo[i-2] + vectorFibo[i-1];
+		}
+		System.out.println("Las diez primeras posiciones de la sucesión de Fibonacci es:");
+		LibreriaFunciones.mostrarVector(vectorFibo);
 	}
 
 }
